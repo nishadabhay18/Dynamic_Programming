c8711673a106c2b46627b87a54b94ebb1c11302a
nishadabhay18 <abhayn927@gmail.com>
2026-05-01 17:24:58 +0530

    init commit

---
+++ src/1277 Count Square Submatrices With All Ones/countSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,15 @@
+class countSquareSubmatricesWithAllOnes {
+    // Tabulation via thinking TC-> O(m*n) SC-> O(1)
+    public int countSquares(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int count = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(matrix[i][j] == 0) continue;
+                if(i > 0 && j > 0) matrix[i][j] += Math.min(matrix[i-1][j], Math.min(matrix[i-1][j-1], matrix[i][j-1]));
+                count += matrix[i][j];
+            }
+        }
+        return count;
+    }
+}
